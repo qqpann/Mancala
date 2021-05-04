97bb6f07dc913b7193e9c38c04875c2e7803e111
Qiushi Pan <17402261+qqhann@users.noreply.github.com>
2021-05-04 16:37:13 +0900

    Update base.py
    Based on the book

--- mancala/state/base.py
+++ mancala/state/base.py
@@ -3,6 +3,20 @@ from builtins import NotImplementedError
 
 
 class BaseState(ABC):
+    def __repr__(self):
+        raise NotImplementedError()
+
+    def clone(self):
+        raise NotImplementedError()
+
+    def __hash__(self):
+        raise NotImplementedError()
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, BaseState):
+            return NotImplemented
+        raise ValueError(other)
+
     @property
     def sided_available_actions(self):
         raise NotImplementedError()
